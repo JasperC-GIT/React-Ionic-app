0c3d56d9437da194b673523de69be9712e3e8b94
JFizzle-git <55044585+JFizzle-git@users.noreply.github.com>
2020-04-18 14:09:56 +1000

    Updates

---
+++ precache-manifest.5280291ee40ce2a28ed58277ec67a8a4.js
@@ -0,0 +1,398 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "957e9ccb49763d68d3d797300c95cc3a",
+    "url": "/React-Ionic-app/index.html"
+  },
+  {
+    "revision": "a4a350300fa90710e049",
+    "url": "/React-Ionic-app/static/css/49.f6437998.chunk.css"
+  },
+  {
+    "revision": "e0717c2945470a234003",
+    "url": "/React-Ionic-app/static/css/main.76e5b857.chunk.css"
+  },
+  {
+    "revision": "43b059d59dacf7725c17",
+    "url": "/React-Ionic-app/static/js/0.3fa666f1.chunk.js"
+  },
+  {
+    "revision": "c4daef51200e00eb2b2c",
+    "url": "/React-Ionic-app/static/js/1.84a02da3.chunk.js"
+  },
+  {
+    "revision": "dd603b98f0bb70c88e85",
+    "url": "/React-Ionic-app/static/js/2.cf5af299.chunk.js"
+  },
+  {
+    "revision": "cead2af04acbdc11f644",
+    "url": "/React-Ionic-app/static/js/3.0416ba0d.chunk.js"
+  },
+  {
+    "revision": "c0aee419127051c9d809",
+    "url": "/React-Ionic-app/static/js/4.7f646ef6.chunk.js"
+  },
+  {
+    "revision": "a4a350300fa90710e049",
+    "url": "/React-Ionic-app/static/js/49.89bdc09c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-Ionic-app/static/js/49.89bdc09c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c1d95b7ccff952f5785",
+    "url": "/React-Ionic-app/static/js/5.fcc34484.chunk.js"
+  },
+  {
+    "revision": "bfcad25e86958497a1e2",
+    "url": "/React-Ionic-app/static/js/50.dbfe3fe1.chunk.js"
+  },
+  {
+    "revision": "90b6b156a5f62810eddc9189ae0e43d4",
+    "url": "/React-Ionic-app/static/js/50.dbfe3fe1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee1b6aa5b07683c7fc30",
+    "url": "/React-Ionic-app/static/js/51.acad6755.chunk.js"
+  },
+  {
+    "revision": "759374b272dd69dfd32c",
+    "url": "/React-Ionic-app/static/js/52.b6e90bfe.chunk.js"
+  },
+  {
+    "revision": "8ad40931045ff0fc57c8",
+    "url": "/React-Ionic-app/static/js/53.228a9526.chunk.js"
+  },
+  {
+    "revision": "c6ae9ccbd5590c8d5fe7",
+    "url": "/React-Ionic-app/static/js/54.ccf21f2e.chunk.js"
+  },
+  {
+    "revision": "bb509dfddfe02c0706be",
+    "url": "/React-Ionic-app/static/js/55.ab0a6546.chunk.js"
+  },
+  {
+    "revision": "6bf7c6d71f24f41ebb6f",
+    "url": "/React-Ionic-app/static/js/56.04ae78c0.chunk.js"
+  },
+  {
+    "revision": "1fdb506cc57a4a8d0e16",
+    "url": "/React-Ionic-app/static/js/57.0df9dad8.chunk.js"
+  },
+  {
+    "revision": "2357321ec408f5b444ec",
+    "url": "/React-Ionic-app/static/js/58.f32ce164.chunk.js"
+  },
+  {
+    "revision": "51c2b1fbd065a6ff5276",
+    "url": "/React-Ionic-app/static/js/59.e4a0dc72.chunk.js"
+  },
+  {
+    "revision": "39667304f2a3ec638bd3",
+    "url": "/React-Ionic-app/static/js/60.cc481dd1.chunk.js"
+  },
+  {
+    "revision": "9044e52f94ea297bd013",
+    "url": "/React-Ionic-app/static/js/61.b21f7e62.chunk.js"
+  },
+  {
+    "revision": "394e638634c3ad4acbd8",
+    "url": "/React-Ionic-app/static/js/62.6da51f6a.chunk.js"
+  },
+  {
+    "revision": "d5ddc59406f61669527e",
+    "url": "/React-Ionic-app/static/js/63.ae975b12.chunk.js"
+  },
+  {
+    "revision": "6121600525918ca97f75",
+    "url": "/React-Ionic-app/static/js/64.5f85ed8d.chunk.js"
+  },
+  {
+    "revision": "dc2cafc5ca3264cb50c3",
+    "url": "/React-Ionic-app/static/js/65.135575da.chunk.js"
+  },
+  {
+    "revision": "f1b369d5d7ac9a8adfe9",
+    "url": "/React-Ionic-app/static/js/66.ceb14aaa.chunk.js"
+  },
+  {
+    "revision": "6651aae9d2422e391822",
+    "url": "/React-Ionic-app/static/js/67.b17a4b74.chunk.js"
+  },
+  {
+    "revision": "ac4fce903281e396d166",
+    "url": "/React-Ionic-app/static/js/68.a41b512e.chunk.js"
+  },
+  {
+    "revision": "27bf390e88a55bb2667e",
+    "url": "/React-Ionic-app/static/js/69.551b9080.chunk.js"
+  },
+  {
+    "revision": "6e9f288281bb2a44814a",
+    "url": "/React-Ionic-app/static/js/70.300f3ab2.chunk.js"
+  },
+  {
+    "revision": "bd5827296fb285f10a87",
+    "url": "/React-Ionic-app/static/js/71.540972bf.chunk.js"
+  },
+  {
+    "revision": "107785d7a85bd9aedb5d",
+    "url": "/React-Ionic-app/static/js/72.49160275.chunk.js"
+  },
+  {
+    "revision": "159f9cace5b1ea2aea48",
+    "url": "/React-Ionic-app/static/js/73.f397ce24.chunk.js"
+  },
+  {
+    "revision": "e61447bd3591abf5d7c3",
+    "url": "/React-Ionic-app/static/js/74.a8c2188a.chunk.js"
+  },
+  {
+    "revision": "18ffeb1a418878048034",
+    "url": "/React-Ionic-app/static/js/75.db55efcb.chunk.js"
+  },
+  {
+    "revision": "a89507995761aa3d581d",
+    "url": "/React-Ionic-app/static/js/76.37be8142.chunk.js"
+  },
+  {
+    "revision": "dcd7f2df4f81b35370f2",
+    "url": "/React-Ionic-app/static/js/77.d134a533.chunk.js"
+  },
+  {
+    "revision": "9353397cd5abd9a1da8f",
+    "url": "/React-Ionic-app/static/js/78.779ca09c.chunk.js"
+  },
+  {
+    "revision": "86ec7fee08be10cc39d9",
+    "url": "/React-Ionic-app/static/js/79.8508f343.chunk.js"
+  },
+  {
+    "revision": "62284f9e1c6eaf94b2a7",
+    "url": "/React-Ionic-app/static/js/80.da4fe1e1.chunk.js"
+  },
+  {
+    "revision": "1938e885e5991c3d4896",
+    "url": "/React-Ionic-app/static/js/81.cd183f8e.chunk.js"
+  },
+  {
+    "revision": "d809d2e8c8558574787a",
+    "url": "/React-Ionic-app/static/js/82.4e4639bc.chunk.js"
+  },
+  {
+    "revision": "3d8ed3d20be713066b17",
+    "url": "/React-Ionic-app/static/js/83.6bbbabc5.chunk.js"
+  },
+  {
+    "revision": "6c39d35a6dcfc8d37ade",
+    "url": "/React-Ionic-app/static/js/84.8a2e249b.chunk.js"
+  },
+  {
+    "revision": "bbc4cdb345d5edaa6f90",
+    "url": "/React-Ionic-app/static/js/85.07f5d9ba.chunk.js"
+  },
+  {
+    "revision": "462b1d33607c512e854f",
+    "url": "/React-Ionic-app/static/js/86.f02ac6dc.chunk.js"
+  },
+  {
+    "revision": "315560a90ef16906ca82",
+    "url": "/React-Ionic-app/static/js/87.38a5cd62.chunk.js"
+  },
+  {
+    "revision": "3665e55e268ba08407aa",
+    "url": "/React-Ionic-app/static/js/88.bad2ac35.chunk.js"
+  },
+  {
+    "revision": "383899e60ed69a9a53414c6836d91ab5",
+    "url": "/React-Ionic-app/static/js/88.bad2ac35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a73a323e2d0a35a264b",
+    "url": "/React-Ionic-app/static/js/89.c8118fc9.chunk.js"
+  },
+  {
+    "revision": "19f46c3d0748a390548b",
+    "url": "/React-Ionic-app/static/js/90.4dc3a154.chunk.js"
+  },
+  {
+    "revision": "d6cd66f13a3e28255cdd",
+    "url": "/React-Ionic-app/static/js/91.dc555883.chunk.js"
+  },
+  {
+    "revision": "383899e60ed69a9a53414c6836d91ab5",
+    "url": "/React-Ionic-app/static/js/91.dc555883.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0717c2945470a234003",
+    "url": "/React-Ionic-app/static/js/main.f3085cbf.chunk.js"
+  },
+  {
+    "revision": "31646d3fd6e3befb69e3",
+    "url": "/React-Ionic-app/static/js/runtime-main.59c738d2.js"
+  },
+  {
+    "revision": "a33c72dbf1faabf452a0",
+    "url": "/React-Ionic-app/static/js/stencil-ion-avatar_3-ios-entry-js.f0050f15.chunk.js"
+  },
+  {
+    "revision": "2ca9a0adf49663090b2b",
+    "url": "/React-Ionic-app/static/js/stencil-ion-avatar_3-md-entry-js.c0f38ad9.chunk.js"
+  },
+  {
+    "revision": "41a0f73c2c9d93c75030",
+    "url": "/React-Ionic-app/static/js/stencil-ion-back-button-ios-entry-js.5f0ffaf9.chunk.js"
+  },
+  {
+    "revision": "2094fadd35f14b58a909",
+    "url": "/React-Ionic-app/static/js/stencil-ion-back-button-md-entry-js.0c4f9726.chunk.js"
+  },
+  {
+    "revision": "c32a8439aa7ff036813d",
+    "url": "/React-Ionic-app/static/js/stencil-ion-backdrop-ios-entry-js.3a17417a.chunk.js"
+  },
+  {
+    "revision": "b46b52ad193445295215",
+    "url": "/React-Ionic-app/static/js/stencil-ion-backdrop-md-entry-js.173007b4.chunk.js"
+  },
+  {
+    "revision": "d99ec22112f400a86f51",
+    "url": "/React-Ionic-app/static/js/stencil-ion-card_5-ios-entry-js.ac3fe2fc.chunk.js"
+  },
+  {
+    "revision": "ae0659beecba74305395",
+    "url": "/React-Ionic-app/static/js/stencil-ion-card_5-md-entry-js.167584f0.chunk.js"
+  },
+  {
+    "revision": "cbcffb12b0fe07fff168",
+    "url": "/React-Ionic-app/static/js/stencil-ion-checkbox-ios-entry-js.a49eba67.chunk.js"
+  },
+  {
+    "revision": "6f0bde46428f3d7a28e1",
+    "url": "/React-Ionic-app/static/js/stencil-ion-checkbox-md-entry-js.83559a02.chunk.js"
+  },
+  {
+    "revision": "84b76196f7cb3edc0bac",
+    "url": "/React-Ionic-app/static/js/stencil-ion-chip-ios-entry-js.c2432b95.chunk.js"
+  },
+  {
+    "revision": "e44ba2f85c469f4004f8",
+    "url": "/React-Ionic-app/static/js/stencil-ion-chip-md-entry-js.c4dac7a4.chunk.js"
+  },
+  {
+    "revision": "cea36ed1a5ce61747290",
+    "url": "/React-Ionic-app/static/js/stencil-ion-col_3-entry-js.a80fe35e.chunk.js"
+  },
+  {
+    "revision": "c3db45e6dcc7572e85cf",
+    "url": "/React-Ionic-app/static/js/stencil-ion-img-entry-js.7c74bd85.chunk.js"
+  },
+  {
+    "revision": "e90ec67b14959ac58590",
+    "url": "/React-Ionic-app/static/js/stencil-ion-infinite-scroll_2-ios-entry-js.6ee5cdd1.chunk.js"
+  },
+  {
+    "revision": "23d4fba24dc3c73bf3ca",
+    "url": "/React-Ionic-app/static/js/stencil-ion-infinite-scroll_2-md-entry-js.c4cbae6b.chunk.js"
+  },
+  {
+    "revision": "f3d888c2d778cd1e9041",
+    "url": "/React-Ionic-app/static/js/stencil-ion-input-ios-entry-js.fc0ba8ee.chunk.js"
+  },
+  {
+    "revision": "76c948b6f878f01f42a5",
+    "url": "/React-Ionic-app/static/js/stencil-ion-input-md-entry-js.3bed06aa.chunk.js"
+  },
+  {
+    "revision": "bb884d092a21d8eb548c",
+    "url": "/React-Ionic-app/static/js/stencil-ion-loading-ios-entry-js.c38d79ce.chunk.js"
+  },
+  {
+    "revision": "4ff90a10a5d765a00f33",
+    "url": "/React-Ionic-app/static/js/stencil-ion-loading-md-entry-js.98d5c69b.chunk.js"
+  },
+  {
+    "revision": "0f424cad7be4ee6a2b42",
+    "url": "/React-Ionic-app/static/js/stencil-ion-popover-ios-entry-js.fe842d00.chunk.js"
+  },
+  {
+    "revision": "559a5ef493455aa677b1",
+    "url": "/React-Ionic-app/static/js/stencil-ion-popover-md-entry-js.889dc84c.chunk.js"
+  },
+  {
+    "revision": "19c5160fe323541ac5b7",
+    "url": "/React-Ionic-app/static/js/stencil-ion-progress-bar-ios-entry-js.d2b46fad.chunk.js"
+  },
+  {
+    "revision": "57072805f4eb0248f69e",
+    "url": "/React-Ionic-app/static/js/stencil-ion-progress-bar-md-entry-js.359aab82.chunk.js"
+  },
+  {
+    "revision": "b39b93aea35f3710ce4d",
+    "url": "/React-Ionic-app/static/js/stencil-ion-radio_2-ios-entry-js.7b9f3db3.chunk.js"
+  },
+  {
+    "revision": "d0b3d70f4e72395d61f3",
+    "url": "/React-Ionic-app/static/js/stencil-ion-radio_2-md-entry-js.79b9ed4c.chunk.js"
+  },
+  {
+    "revision": "e70a7dcd8e3c0a947649",
+    "url": "/React-Ionic-app/static/js/stencil-ion-reorder_2-ios-entry-js.4c12c9a5.chunk.js"
+  },
+  {
+    "revision": "723955bb1a1c63b3605c",
+    "url": "/React-Ionic-app/static/js/stencil-ion-reorder_2-md-entry-js.4e3c9390.chunk.js"
+  },
+  {
+    "revision": "7da93f4afcbd85b8625d",
+    "url": "/React-Ionic-app/static/js/stencil-ion-ripple-effect-entry-js.768fe67c.chunk.js"
+  },
+  {
+    "revision": "2956a1ba7e44323d95c3",
+    "url": "/React-Ionic-app/static/js/stencil-ion-spinner-entry-js.612d019e.chunk.js"
+  },
+  {
+    "revision": "a3f8c5fcba980173b47b",
+    "url": "/React-Ionic-app/static/js/stencil-ion-split-pane-ios-entry-js.1ce3272a.chunk.js"
+  },
+  {
+    "revision": "fc5b9a40f75cbf3bb4cb",
+    "url": "/React-Ionic-app/static/js/stencil-ion-split-pane-md-entry-js.c963717e.chunk.js"
+  },
+  {
+    "revision": "78272a72bca35fdab3cb",
+    "url": "/React-Ionic-app/static/js/stencil-ion-tab-bar_2-ios-entry-js.2ba3ad76.chunk.js"
+  },
+  {
+    "revision": "9b19ca68233280a18055",
+    "url": "/React-Ionic-app/static/js/stencil-ion-tab-bar_2-md-entry-js.af54e54e.chunk.js"
+  },
+  {
+    "revision": "84a36d623249bc1c1930",
+    "url": "/React-Ionic-app/static/js/stencil-ion-tab_2-entry-js.901fb409.chunk.js"
+  },
+  {
+    "revision": "a891e3b59e715828629f",
+    "url": "/React-Ionic-app/static/js/stencil-ion-text-entry-js.00f3cfb7.chunk.js"
+  },
+  {
+    "revision": "5810252f1c184bea30dc",
+    "url": "/React-Ionic-app/static/js/stencil-ion-textarea-ios-entry-js.e5d94dc7.chunk.js"
+  },
+  {
+    "revision": "a2811eacb9ea9ed112a2",
+    "url": "/React-Ionic-app/static/js/stencil-ion-textarea-md-entry-js.9cb3869c.chunk.js"
+  },
+  {
+    "revision": "efb598801ba97fe111bf",
+    "url": "/React-Ionic-app/static/js/stencil-ion-toggle-ios-entry-js.6773de1e.chunk.js"
+  },
+  {
+    "revision": "e7c259c464ed1dfb4149",
+    "url": "/React-Ionic-app/static/js/stencil-ion-toggle-md-entry-js.7258ca24.chunk.js"
+  },
+  {
+    "revision": "8dd404c043725cff5aa6",
+    "url": "/React-Ionic-app/static/js/stencil-ion-virtual-scroll-entry-js.02052537.chunk.js"
+  }
+]);
